f5ee15353a77d00a258835928d1585252a0b5898
Thomas Roch <thomas.c.roch@gmail.com>
2016-02-28 15:06:02 +0000

    chore: v3.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+<a name="3.0.0"></a>
+# [3.0.0](https://github.com/router5/router5-persistent-params/compare/v1.1.3...v3.0.0) (2016-02-28)
+
+
+
+
 <a name="1.1.3"></a>
 ## [1.1.3](https://github.com/router5/router5-persistent-params/compare/v1.1.2...v1.1.3) (2016-01-25)
 
---
+++ dist/es/index.js
@@ -0,0 +1,51 @@
+var getDefinedParams = function getDefinedParams(params) {
+    return Object.keys(params).filter(function (param) {
+        return params[param] !== undefined;
+    }).reduce(function (acc, param) {
+        return babelHelpers.extends({}, acc, babelHelpers.defineProperty({}, param, params[param]));
+    }, {});
+};
+
+var persistentParamsPlugin = function persistentParamsPlugin() {
+    var params = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    return function (router) {
+        // Persistent parameters
+        var persistentParams = Array.isArray(params) ? params.reduce(function (acc, param) {
+            return babelHelpers.extends({}, acc, babelHelpers.defineProperty({}, param, undefined));
+        }, {}) : params;
+
+        var paramNames = Object.keys(persistentParams);
+
+        // Root node path
+        var path = router.rootNode.path.split('?')[0] + paramNames.length ? '?' + paramNames.join('&') : '';
+        router.rootNode.setPath(path);
+
+        var buildPath = router.buildPath;
+        var buildState = router.buildState;
+
+        // Decorators
+
+        router.buildPath = function (route, params) {
+            var routeParams = babelHelpers.extends({}, getDefinedParams(persistentParams), params);
+            return buildPath.call(router, route, routeParams);
+        };
+
+        router.buildState = function (route, params) {
+            var routeParams = babelHelpers.extends({}, getDefinedParams(persistentParams), params);
+            return buildState.call(router, route, routeParams);
+        };
+
+        return {
+            name: 'PERSISTENT_PARAMS',
+            onTransitionSuccess: function onTransitionSuccess(toState) {
+                Object.keys(toState.params).filter(function (p) {
+                    return paramNames.indexOf(p) !== -1;
+                }).forEach(function (p) {
+                    return persistentParams[p] = toState.params[p];
+                });
+            }
+        };
+    };
+};
+
+export default persistentParamsPlugin;
